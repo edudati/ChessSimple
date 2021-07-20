ca63b0c01c967eb41c1dac958f7d691be6e13e59
Eduardo Dati <edudati@gmail.com>
2021-07-20 16:07:30 -0300

    implement mehtod to check if is a valid position, and if there is another piece in the position. Creation of exceptions: invalid position, and exist another piece in the position

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -10,15 +10,33 @@ namespace Chess
         {
             Board b = new Board(8, 8);
 
-            b.putPiece(new King(b, Color.Black), new Position(0, 4));
-            b.putPiece(new Rock(b, Color.Black), new Position(0, 0));
-            b.putPiece(new Rock(b, Color.Black), new Position(0, 7));
+            try
+            {
+                b.putPiece(new King(b, Color.Black), new Position(0, 4));
+                b.putPiece(new Rock(b, Color.Black), new Position(0, 0));
+                b.putPiece(new Rock(b, Color.Black), new Position(0, 7));
+                
+                /* test for same position exception: 
+                b.putPiece(new Rock(b, Color.Black), new Position(0, 7));
+                b.putPiece(new Rock(b, Color.Black), new Position(0, 7));
+                */
+                /* test for invalid position exception:
+                b.putPiece(new Rock(b, Color.Black), new Position(0, 9));
+                */
 
-            b.putPiece(new King(b, Color.White), new Position(7, 4));
-            b.putPiece(new Rock(b, Color.White), new Position(7, 0));
-            b.putPiece(new Rock(b, Color.White), new Position(7, 7));
+                b.putPiece(new King(b, Color.White), new Position(7, 4));
+                b.putPiece(new Rock(b, Color.White), new Position(7, 0));
+                b.putPiece(new Rock(b, Color.White), new Position(7, 7));
 
-            Screen.printBoard(b);
+                Screen.printBoard(b);
+            }
+            catch (BoardException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            
         }
     }
 }
--- Chess/board/Board.cs
+++ Chess/board/Board.cs
@@ -19,11 +19,48 @@
             return pieces[row, col];
         }
 
-        // put one piece on the board (array of pieces) and put position to the piece p
+        // Allow access to other classes to private piece from a Position... return an especific piece on the array
+        public Piece piece(Position pos)
+        {
+            return pieces[pos.row, pos.col];
+        }
+
+        // put one piece on the board (array of pieces) and put position to the piece p, 
         public void putPiece(Piece p, Position pos)
         {
+            //exception if exist some piece in the position
+            if (existPiece(pos))
+            {
+                throw new BoardException("There is another piece in the same place.");
+            }
             pieces[pos.row, pos.col] = p;
             p.position = pos;   
+        }
+
+        // This method validate if the position is valid and check is exist a piece in this position
+        public bool existPiece(Position pos)
+        {
+            validatePosition(pos);
+            return piece(pos) != null;
+        }
+        
+        // Check if the position is valid
+        public bool isValidPosition(Position pos)
+        {
+            if (pos.row < 0 || pos.row >= rows || pos.col < 0 || pos.col >= cols)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //validate if is a valid position
+        public void validatePosition(Position pos)
+        {
+            if (!isValidPosition(pos))
+            {
+                throw new BoardException("Invalid position.");
+            }
         }
     }
 }
